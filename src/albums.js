5f97ac1fb2231e53c96b9c447202b0d11f96a905
suvash <suvash2077@gmail.com>
2023-01-15 16:29:30 +0545

    Images Added

---
+++ src/albums.js
@@ -0,0 +1,58 @@
+const albumData = [
+  {
+    name: "Dark Side of the Moon",
+    src: "./images/dsom.jpg",
+  },
+  {
+    name: "Animals",
+    src: "./images/animals.jpg",
+  },
+  {
+    name: "Atom Heart Mother",
+    src: "./images/atom.jpg",
+  },
+  {
+    name: "The Division Bell",
+    src: "./images/divbell.jpg",
+  },
+  {
+    name: "The Endless River",
+    src: "./images/endriver.jpg",
+  },
+  {
+    name: "The Final Cut",
+    src: "./images/final.jpg",
+  },
+  {
+    name: "Meddle",
+    src: "./images/meddle.jpg",
+  },
+  {
+    name: "More",
+    src: "./images/more.jpg",
+  },
+  {
+    name: "The Piper at the Gates Dawn",
+    src: "./images/piper.jpg",
+  },
+  {
+    name: "A Saucerful of Secrets",
+    src: "./images/saucer.jpg",
+  },
+  {
+    name: "Ummagumma",
+    src: "./images/umma.jpg",
+  },
+  {
+    name: "The Wall",
+    src: "./images/wall.jpg",
+  },
+  {
+    name: "Wish you were here",
+    src: "./images/wish.jpg",
+  },
+];
+
+
+
+export default albumData;
---
+++ src/components/Game.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import Overview from "./Overview";
+
+function Game() {
+  const [score, setScore] = useState(0);
+  return <div>Game</div>;
+}
+
+export default Game;
