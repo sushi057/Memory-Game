a30e37b069b69e77c9e8403c9eaae100eab395fc
suvash <suvash2077@gmail.com>
2023-01-21 19:03:38 +0545

    Score System

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -5,15 +5,26 @@ import "../styles/albums.css";
 const Overview = () => {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
+  const [albums, setAlbums] = useState([]);
 
   const setNewHighScore = () => {
     if (score > highScore) setHighScore(score);
   };
 
+  const handleClick = (e) => {
+    if (albums.includes(e.target.alt)) {
+      console.log("something");
+      setScore(0);
+      setAlbums([]);
+    } else {
+      setScore(score + 1);
+      setAlbums((prev) => [...prev, e.target.alt]);
+    }
+  };
   useEffect(() => {
     setNewHighScore();
-    console.log("useEffect");
-  }, [score]);
+    console.log(albums);
+  }, [score, albums]);
 
   return (
     <div>
@@ -25,11 +36,7 @@ const Overview = () => {
         {albumsData.map((album) => {
           return (
             <li>
-              <img
-                src={album.src}
-                alt={album.name}
-                onClick={() => setScore(score + 1)}
-              />
+              <img src={album.src} alt={album.name} onClick={handleClick} />
               <p>{album.name}</p>
             </li>
           );
